436c8e180d4755fa43521dcf8d1b1bf22176217f
sobarine21 <sobarinefinance@gmail.com>
2025-09-22 21:31:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1625,4 +1625,3 @@ with tab_inst:
         st.dataframe(df_instruments.head(200), use_container_width=True)
     else:
         st.info("No instruments loaded. Click 'Load Instruments for Selected Exchange' above to fetch.")
-```
